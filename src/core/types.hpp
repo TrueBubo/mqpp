b4f30b5964f86924f13e25889767afccce5582c9
Filip Bubak <bubak.filip@pm.me>
2026-02-12 17:04:02 +0100

    Add type definitions

---
+++ src/core/types.hpp
@@ -0,0 +1,16 @@
+#ifndef TYPES_HPP
+#define TYPES_HPP
+
+#include <string>
+#include <functional>
+
+namespace mqpp {
+
+class Message;
+
+using MessageId = std::string;  // UUID
+
+using MessageHandler = std::function<void(const Message&)>;
+
+}  // namespace mqpp
+#endif // TYPES_HPP
