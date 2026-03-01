2a4752902a0c6aee400cf92dd84d27a212aed669
Filip Bubak <bubak.filip@pm.me>
2026-03-01 22:56:53 +0100

    Fix message creation

--- src/core/message.cpp
+++ src/core/message.cpp
@@ -2,6 +2,7 @@
 #include "string_utils.hpp"
 #include <uuid.h>
 #include <random>
+#include <algorithm>
 
 namespace mqpp {
 
@@ -13,7 +14,7 @@ namespace {
     std::string generate_uuid() {
         std::random_device rd;
         auto seed_data = std::array<int, std::mt19937::state_size>{};
-        std::ranges::generate(seed_data, std::ref(rd));
+        std::generate(seed_data.begin(), seed_data.end(), std::ref(rd));
         std::seed_seq seq(std::begin(seed_data), std::end(seed_data));
         std::mt19937 generator(seq);
         uuids::uuid_random_generator gen{generator};
