50473cc52ff23038e2c756f6f050b77c3953441f
Filip Bubak <bubak.filip@pm.me>
2026-03-03 19:25:00 +0100

    Simplify UUID generation

--- src/util/uuid_util.hpp
+++ src/util/uuid_util.hpp
@@ -2,7 +2,6 @@
 #define MQ_UUID_UTIL_HPP
 
 #include <uuid.h>
-#include <algorithm>
 
 namespace mqpp {
 
@@ -13,13 +12,9 @@ public:
      * @return String form of the generated UUID
      */
     static std::string generate() {
-        std::random_device rd;
-        auto seed_data = std::array<int, std::mt19937::state_size>{};
-        std::generate(seed_data.begin(), seed_data.end(), std::ref(rd));
-        std::seed_seq seq(std::begin(seed_data), std::end(seed_data));
-        std::mt19937 generator(seq);
-        uuids::uuid_random_generator gen{generator};
-
+        size_t seed = std::random_device()();
+        std::mt19937 generator(seed);
+        uuids::uuid_random_generator gen(generator);
         return uuids::to_string(gen());
     }
 };
